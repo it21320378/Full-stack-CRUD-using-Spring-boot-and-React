3814aa9aaca89f8b0bdedf7ee631c2d69fab8100
it21320378 <it21320378@my.sliit.lk>
2024-04-03 09:20:27 +0530

    done!

--- fullstack-backend/src/main/java/com/virajsg/fullstackbackend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.virajsg.fullstackbackend.repository;
-
-import com.virajsg.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
