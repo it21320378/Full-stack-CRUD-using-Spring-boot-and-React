e086ba37472dd232589337377e10f8114034af3a
it21320378 <it21320378@my.sliit.lk>
2024-04-03 09:28:18 +0530

    add springboot initializer

---
+++ fullstack-backend/src/test/java/com/virajsg0378/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virajsg0378.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
