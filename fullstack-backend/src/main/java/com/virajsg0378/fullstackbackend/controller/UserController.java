1ef4456d4926d449b467e9c9032d95a39304c6f6
it21320378 <it21320378@my.sliit.lk>
2024-04-04 11:35:55 +0530

    display info in Webapp from DB, using Axios GET

--- fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.virajsg0378.fullstackbackend.controller;
 import com.virajsg0378.fullstackbackend.model.User;
 import com.virajsg0378.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 
     @Autowired
