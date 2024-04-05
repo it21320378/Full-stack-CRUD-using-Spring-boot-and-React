ed66ab66980bc7f6813e72099a1bede2e45a629f
it21320378 <it21320378@my.sliit.lk>
2024-04-05 21:38:01 +0530

    backend springboot mapping for edit user for specific id, using PutMapping

--- fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/exception/UserNotFoundAdvice.java
+++ fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/exception/UserNotFoundAdvice.java
@@ -14,7 +14,7 @@ public class UserNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)git add 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String,String> exceptionHandler(UserNotFoundException exception){
 
         Map<String,String> errorMap=new HashMap<>();
