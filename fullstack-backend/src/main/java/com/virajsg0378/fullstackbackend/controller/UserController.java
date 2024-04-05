9c29bf2eeb3541d6d398194106036da793865fa5
it21320378 <it21320378@my.sliit.lk>
2024-04-05 19:49:56 +0530

    custom exception handling in backend, to show specific user info for the given user id

--- fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/virajsg0378/fullstackbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.virajsg0378.fullstackbackend.controller;
 
+import com.virajsg0378.fullstackbackend.exception.UserNotFoundException;
 import com.virajsg0378.fullstackbackend.model.User;
 import com.virajsg0378.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 }
